dd68417df5bb7387cbf9bf686e8468129c9b33dc
Lex Chou <lex@chou.it>
2016-06-07 20:22:08 +0800

    Emacs plugins upgraded

--- elpa/edbi-20140920.35/edbi-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; edbi-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "edbi" "edbi.el" (21807 50603 0 0))
-;;; Generated autoloads from edbi.el
-
-(autoload 'edbi:open-db-viewer "edbi" "\
-Open Database viewer buffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("edbi-pkg.el") (21807 50603 655353 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; edbi-autoloads.el ends here
---
+++ elpa/term+-20160404.355/term+-pkg.el
@@ -0,0 +1,5 @@
+(define-package "term+" "20160404.355" "term-mode enhancement" 'nil :url "https://github.com/tarao/term-plus-el" :keywords
+  '("terminal" "emulation"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
